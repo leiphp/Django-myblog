53b57cfcc63c5dcf32ee0dda5b44f1e32b63d914
leiphp <tian-lei-happy@163.com>
2018-12-28 18:39:38 +0800

    add url

--- myblog/urls.py
+++ myblog/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-import blog.views as bv
+from django.conf.urls import url, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', bv.index),
+    # path('index/', include('blog.urls')),
+    url(r'^blog/',include('blog.urls')),
 ]
