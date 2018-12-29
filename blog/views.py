2ea401bead36ef87d12dbe4e129a9b67dc32134f
leiphp <tian-lei-happy@163.com>
2018-12-29 16:02:07 +0800

    add models

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello, World!')
-    return render(request, 'blog/index.html',{'hello':'Hello，Blog!'})
+    # return render(request, 'blog/index.html',{'hello':'Hello，Blog!'})
+
+    article=models.Article.objects.get(pk=1)#传递article对象
+    return render(request, 'blog/index.html',{'article':article})
