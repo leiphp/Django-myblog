69a3a6f26c03067786c9b3fc5b7e2601c9d677f0
leiwen <657106593@qq.com>
2019-01-07 00:08:44 +0800

    add templates file

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Article(models.Model):
     title = models.CharField(max_length=32, default='Title')
     content = models.TextField(null=True)
     pub_time = models.DateTimeField(null=True)
+    pub_img = models.CharField(max_length=32)
 
     def __str__(self):
         return self.title
